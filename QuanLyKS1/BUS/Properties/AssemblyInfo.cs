5d4f2110cef31adb11edb103a4f48812b4e7300f
ThaoGitHub <thaonb18@gmail.com>
2017-05-30 10:56:51 +0700

    update project 30-05
    quan ly khach san

--- QuanLyKS1/BUS/Properties/AssemblyInfo.cs
+++ QuanLyKS1/BUS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataAccess")]
+[assembly: AssemblyTitle("BUS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAccess")]
+[assembly: AssemblyProduct("BUS")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a88e8687-7976-43bd-93f2-fe26daa0abdb")]
+[assembly: Guid("7a2ce331-933a-4b4e-b19f-10f1f2c730ff")]
 
 // Version information for an assembly consists of the following four values:
 //
